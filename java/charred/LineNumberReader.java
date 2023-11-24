385eaf7975f67f05bb696e9cbd9f14d3b1c24585
Chris Nuernberger <chris@techascent.com>
2023-11-24 11:19:50 -0700

    Higher performance replacement for pushbackReader and lineNumberReader.

--- java/charred/LineNumberReader.java
+++ java/charred/LineNumberReader.java
@@ -4,28 +4,29 @@ package charred;
 import java.io.Reader;
 import java.io.IOException;
 
+
 public class LineNumberReader {
   final char[] buffer;
   final Reader reader;
   int position = 0;
   int len = 0;
-  int line = 0;
-  int column = 0;
+  int line = 1;
+  int column = 1;
   int lastColumn = 0;
   public LineNumberReader(Reader r, int buflen) {
     this.reader = r;
     this.buffer = new char[buflen];
   }
   public LineNumberReader(Reader r) {
-    this(r, 2056)
+    this(r, 2056);
   }
-  public int line() { return this.line; }
-  public int column() { return this.column; }
+  public int getLineNumber() { return this.line; }
+  public int getColumnNumber() { return this.column; }
   public char update(char b) {
     if(b == '\n') {
       ++line;
       lastColumn = column;
-      column = 0;
+      column = 1;
     } else {
       ++column;
     }
@@ -41,21 +42,29 @@ public class LineNumberReader {
   public void unread() {
     if(position == 0 || len == 0)
       throw new RuntimeException("Too many unread ops.");
-    position--;
-    if(buffer[position] == '\n') {
-      --line;
-      column = lastColumn;
+    //unread on eof is a noop
+    if(len != -1) {
+      position--;
+      if(buffer[position] == '\n') {
+	--line;
+	column = lastColumn;
+      } else {
+	--column;
+      }
     }
   }
   
   public char[] buffer() throws IOException {
+    if(len == -1)
+      return null;
     if(position == len) {
       len = reader.read(buffer, 0, buffer.length-1);
       if(len == -1)
 	return null;
       int writepos = 0;
       boolean cret = false;
-      for(int idx = 0; idx < len; ++idx) {
+      final int l = len;
+      for(int idx = 0; idx < l; ++idx) {
 	final char c = buffer[idx];
 	switch(c) {
 	case '\r':
